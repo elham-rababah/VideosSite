5ac1f0210031ba8b80b05f6763ce44321e4cbe97
Elhamrababah <Elham.a.rababah@gmail.com>
2018-03-15 14:11:29 +0200

    implement http req

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { SafePipe } from './utility/safe.pipe';
   imports: [
     AppRouteingModule,
     BrowserModule,
+    HttpModule,
     
   ],
   providers: [],
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,37 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
 	selector: 'app-video-list',
 	templateUrl: './video-list.component.html',
 	styleUrls: ['./video-list.component.css']
 })
-export class VideoListComponent implements OnInit {
-	title ="video-list works!"
-	//videoList = ['item1','item2','item3'];
-	videoList = [
-		{
-			name: "item1",
-			id: "LuwP0RBnPrY"
-		},
-		{
-			name: "item2",
-			id: "tnjcofZfBMs"
-		},
-		{
-			name: "item3",
-			id: "FDX44Uavneo"
-		},
-		{
-			name: "item4",
-			id: "aUjQv8QCQaM"
-		},
-	]
+export class VideoListComponent implements OnInit{
+	title ="video-list works!";
+	private req: any;
+	private videoList: [any];
+
+	constructor(private http: Http) { }
 
 	ngOnInit() {
+		this.req = this.http.get('assets/json/video.json')
+		.subscribe(data => {
+			this .videoList = data.json();
+		});
 	}
 
 	generateEmbedUrl (id :string){
 		return 'https://www.youtube.com/embed/' + id;
+	}
+
+	ngOnDestroy(){
+		this.req.unsubscribe();
 	}
 
 }
